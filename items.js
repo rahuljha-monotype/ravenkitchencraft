4b8a590b502db07963d9a2a4094fbb6ae99e0234
rahuljha-monotype <rahul.jha@monotype.com>
2023-12-21 13:25:05 +0530

    static pg created

---
+++ items.js
@@ -0,0 +1,10 @@
+// items.js
+const items = [
+    { id: 1, images: ['images/item1/photo1.jpg'], name: 'Item 1 name', description: 'desc1',  whatsappLink: 'https://wa.me/9870311304' },
+    { id: 2, images: ['images/item1/photo2.jpg'], name: 'Item 2 name',  description: 'desc1', whatsappLink: 'https://wa.me/9870311304' },
+    { id: 3, images: ['images/item2/photo3.jpg'], name: 'Item 3 name',  description: 'desc1', whatsappLink: 'https://wa.me/9870311304' },
+    { id: 4, images: ['images/item2/photo4.jpg'], name: 'Item 4 name',  description: 'desc1', whatsappLink: 'https://wa.me/9870311304' },
+    { id: 5, images: ['images/item3/photo5.jpg'], name: 'Item 5 name',  description: 'desc1', whatsappLink: 'https://wa.me/9870311304' },
+    { id: 6, images: ['images/item3/photo6.jpg'], name: 'Item 6 name',  description: 'desc1', whatsappLink: 'https://wa.me/9870311304' },
+    // Add more items as needed
+  ];
